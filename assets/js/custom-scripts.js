2ba583e6a718faffd3424dbbc49f1fc0543482ae
Paul Ryan <pwryan81@hotmail.com>
2012-05-03 18:57:10 +1000

    First commit.
    Signed-off-by: Paul Ryan <pwryan81@hotmail.com>

---
+++ assets/js/custom-scripts.js
@@ -0,0 +1,41 @@
+// JavaScript Document
+
+/* Customise default JQM behaviour */
+/* http://jquerymobile.com/demos/1.1.0/docs/api/globalconfig.html */
+
+$(document).bind("mobileinit", function(){
+  //apply overrides here
+  //$.mobile.ajaxEnabled = true;
+  //$.mobile.allowCrossDomainPages = true;
+  
+  //$.mobile.defaultDialogTransition = 'pop';
+  //$.mobile.defaultPageTransition = 'fade';
+  //$.mobile.allowCrossDomainPages = true;
+  //$.mobile.allowCrossDomainPages = true;
+  //$.mobile.allowCrossDomainPages = true;
+  //$.mobile.allowCrossDomainPages = true;
+  
+   // Navigation
+   /*
+    $.mobile.page.prototype.options.backBtnText = "Back";
+    $.mobile.page.prototype.options.addBackBtn      = false;
+    $.mobile.page.prototype.options.backBtnTheme    = "b";
+	*/
+    // Page  
+	/*
+    $.mobile.page.prototype.options.headerTheme = "b";  // Page header only
+    $.mobile.page.prototype.options.contentTheme = "b";
+    $.mobile.page.prototype.options.footerTheme = "b";
+
+    // Listviews
+    $.mobile.listview.prototype.options.headerTheme = "b";  // Header for nested lists
+    $.mobile.listview.prototype.options.theme           = "b";  // List items / content
+    $.mobile.listview.prototype.options.dividerTheme    = "b";  // List divider
+
+    $.mobile.listview.prototype.options.splitTheme   = "b";
+    $.mobile.listview.prototype.options.countTheme   = "b";
+    $.mobile.listview.prototype.options.filterTheme = "b";
+    $.mobile.listview.prototype.options.filterPlaceholder = "Filter data...";
+	*/
+  
+});
