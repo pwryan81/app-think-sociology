2127b8dfbbb845aca82a31dcd80b648964512548
Paul Ryan <pwryan81@hotmail.com>
2012-05-08 23:25:28 +1000

    Updated 09/05/2012
    Signed-off-by: Paul Ryan <pwryan81@hotmail.com>

--- assets/js/custom-scripts.js
+++ assets/js/custom-scripts.js
@@ -1,30 +1,36 @@
 // JavaScript Document
-
 /* Customise default JQM behaviour */
 /* http://jquerymobile.com/demos/1.1.0/docs/api/globalconfig.html */
+/* http://jquerymobile.com/demos/1.1.0/docs/api/events.html */
+/* http://jquerymobile.com/demos/1.1.0/docs/api/methods.html */
+/* http://jquerymobile.com/demos/1.1.0/docs/api/data-attributes.html */
+/* http://jquerymobile.com/demos/1.1.0/docs/pages/phonegap.html */
 
+$(document).bind("mobileinit", function () {
+    //apply overrides here
+    //$.mobile.ajaxEnabled = false;
+    //$.mobile.allowCrossDomainPages = true;
+    //$.mobile.defaultDialogTransition = 'pop';
+     $.mobile.defaultPageTransition = 'none';  //none is best for mobile, cos transition is not too smooth.
+    //$.mobile.defaultPageTransition = 'fade';  // Fade is best for desktop
 
-$(document).bind("mobileinit", function(){
-  //apply overrides here
-  //$.mobile.ajaxEnabled = false;
-  //$.mobile.allowCrossDomainPages = true;
-  
-  //$.mobile.defaultDialogTransition = 'pop';
-  $.mobile.defaultPageTransition = 'none';  //none, fade, slide, pop
+    //Set the delay for touch devices to add the hover and down classes on touch interactions for buttons throughout the framework. Reducing the delay here results in a more responsive feeling ui, but will often result in the downstate being applied during page scrolling.
+    $.mobile.buttonMarkup.hoverDelay = 0;
 
-   //Set the delay for touch devices to add the hover and down classes on touch interactions for buttons throughout the framework. Reducing the delay here results in a more responsive feeling ui, but will often result in the downstate being applied during page scrolling.
-	 $.mobile.buttonMarkup.hoverDelay = 0;
-  
-   // Navigation
-
+    // Navigation
     $.mobile.page.prototype.options.backBtnText = "Back";
-    $.mobile.page.prototype.options.addBackBtn      = true;
-    $.mobile.page.prototype.options.backBtnTheme    = "b";
+    $.mobile.page.prototype.options.addBackBtn = true;
+    $.mobile.page.prototype.options.backBtnTheme = "b";
 
+    $.mobile.loadingMessage = "Thinking..."
+    $.mobile.pageLoadErrorMessage = "Oops! Page not found.";
 	
+	/*  in PhoneGap apps that must "phone home" by loading assets off a remote server, both the $.support.cors AND $.mobile.allowCrossDomainPages must be set to true.  */
+	$.support.cors = true;
+	$.mobile.allowCrossDomainPages = true;
 	
-    // Page  
 	/*
+    // Page  
     $.mobile.page.prototype.options.headerTheme = "b";  // Page header only
     $.mobile.page.prototype.options.contentTheme = "b";
     $.mobile.page.prototype.options.footerTheme = "b";
@@ -34,32 +40,54 @@ $(document).bind("mobileinit", function(){
     $.mobile.listview.prototype.options.headerTheme = "b";  // Header for nested lists
     $.mobile.listview.prototype.options.theme           = "b";  // List items / content
     $.mobile.listview.prototype.options.dividerTheme    = "b";  // List divider
-
     $.mobile.listview.prototype.options.splitTheme   = "b";
     $.mobile.listview.prototype.options.countTheme   = "b";
     $.mobile.listview.prototype.options.filterTheme = "b";
     $.mobile.listview.prototype.options.filterPlaceholder = "Filter data...";
 	*/
+
+
+
+});
+
+$(document).delegate("#glossary", "pageinit", function () {
+	//	alert('Glossary inited');
 	
+	$("select").bind("change", function (event, ui) {
+	   var filterValue = $("select option:selected").val();
+	    //alert("Filtering on " + filterValue );
+		$(".ui-input-text").val(filterValue).keyup(); //keyup to refresh filter
+    });
 	
-  
+});
+
+$(document).delegate("#key-terms", "pageinit", function () {
+    //alert('A page with an ID of "key-terms" was just created by jQuery Mobile!');
+    /* Function to expand all or collapse all collabsibles when radio button toggled*/
+
+    //$("#collapsibleGroup input[type='radio']").bind( "change", function(event, ui) {
+    $("#collapsibleGroup input[type='radio']").live("change", function (event, ui) {
+        //alert("toggle to " + this.value );
+        $('.ui-collapsible').trigger(this.value);
+    });
 });
 
 
-$(document).ready(function(){ 
-  
-/* Function to expand all or collapse all collabsibles when radio button toggled*/
-	$("#collapsibleGroup input[type='radio']").bind( "change", function(event, ui) {
-		//alert("toggle");
-		$('.ui-collapsible').trigger(this.value);
-	});
-	
-	/*
-	// Function to scroll to top
-	*/
-	$("a.scrollToTop").click(function(){
-	  //alert("Scrolling to top");
-    $("body").scrollTop(0);
-  });
-	
+
+$(document).bind("pageinit", function () {
+
+    // Function to scroll to top
+    $("a[rel='top']").click(function () {
+
+        //$("body").scrollTop(0);
+        $.mobile.silentScroll(0);
+    });
+
+});
+
+
+// Orientation - Doesn't seem to work
+$(document).bind("orientationchange", function () {
+    alert("orientation changed");
+    alert(event.orientation);
 });
---
+++ sociological-theory/quiz-ch2.js
@@ -0,0 +1,52 @@
+﻿var init = { 
+     "questions": [ 
+       {
+           "question": "Survival of the fittest describes __________.",
+           "answers": [
+		   "social Darwinism",
+		   "common sense theory",
+		   "functionalism",
+		   "post-structuralist theory"],
+			  "correctAnswer": 1
+       },
+       {
+           "question": "Functionalism is a theoretical framework that defines society as ___________.",
+           "answers": [
+		   "manifest functions",
+		   "latent functions",
+		   "a series of paradigms",
+		   "a system of interrelated parts"],
+			  "correctAnswer": 4
+       },
+		 {
+           "question": "The framework that views society as being in a constant struggle over scarce resources is called ______.",
+           "answers": [
+		   "functionalism",
+		   "conflict theory",
+		   "symbolic interactionism",
+		   "social statics?"],
+			  "correctAnswer": 2
+       },
+	    {
+           "question": "______ is primarily a micro approach because it is concerned with the individual's role in creating society.",
+           "answers": [
+		   "Conflict theory",
+		   "Symbolic interactionism",
+		   "Integration",
+		   "Social exchange theory"],
+			  "correctAnswer": 2
+       },
+	   
+     ],
+	  "resultComments" :  
+	  {
+		    perfect: "A master of Socialogical Theory.",
+			 excellent: "Outstanding knowledge on Socialogical Theory!",
+			 good: "Exceeds expectations!",
+			 average: "Acceptable.",
+			 bad: "Well, that was poor.",
+			 poor: "Perhaps you need to actually buy the textbook from MyPearsonStore!",
+			 worst: "Fail!"
+	  }
+
+ };
